7b11bad917d428add88cceca77cc8ccecdfe06f4
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-16 11:06:42 +0200

    feat:add project SkipTakeSum

---
+++ SkipTakeSum/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<double> { 1.02, 2.1, 3.5, 0.4, 53, 6.0, 7.6, 8.5, 9.2 };
+        var skipTakeSum = new SkipTakeSumAvg();
+        skipTakeSum.Sum(numbers);
+        skipTakeSum.Avg(numbers);
+    }
+}
---
+++ SkipTakeSum/SkipTakeSumAvg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SkipTakeSumAvg
+{
+    public void Sum(List<double> numbers)
+    {
+        Console.Write("Enter the skip value: ");
+        int skipValue = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the take value: ");
+        int takeValue = int.Parse(Console.ReadLine());
+
+        var sum = numbers.Skip(skipValue).Take(takeValue).Sum();
+        Console.WriteLine($"Sum :{sum}");
+    }
+
+    public void Avg(List<double> numbers)
+    {
+        Console.WriteLine("Enter skip value :");
+        int skipValue = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter take value:");
+        int takeValue = int.Parse(Console.ReadLine());
+        var average = numbers.Skip(skipValue).Take(takeValue).Average();
+        Console.WriteLine($"Average :{average}");
+    }
+}
